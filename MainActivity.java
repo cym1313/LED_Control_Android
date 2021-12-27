cad8a97865ba31bb89bd0e9d2adc73285ff050fd
cym1313 <75101848+cym1313@users.noreply.github.com>
2021-12-28 03:46:03 +0900

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,76 @@
+package com.example.led_control;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    Button onButton;
+    Button offButton;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        onButton = findViewById(R.id.btn01);
+        offButton = findViewById(R.id.btn02);
+        textView = findViewById(R.id.textView);
+        setTitle(("LED Remote Control"));
+
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("LED_STATUS");
+
+        myRef.setValue("Test");
+        textView.setText(myRef.getKey());
+
+        // read from the Database, addValue Event Listenning
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String ledState = (String) dataSnapshot.getValue();
+                //String value = dataSnapshot.getValue(String.class);
+                textView.setText("LED is " + ledState);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        onButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setBackgroundColor(Color.YELLOW);
+                // write to the Database
+                myRef.setValue("ON");
+            }
+        });
+
+        offButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setBackgroundColor(Color.GREEN);
+                // write to the Database
+                myRef.setValue("OFF");
+            }
+        });
+    }
+}
